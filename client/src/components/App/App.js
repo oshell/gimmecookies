9380204642fd07b18f9971784de4e79cd7d7f337
Horst Jahns <horstjahns@gmail.com>
2018-11-29 22:59:01 +0100

    - added bootstrap and sass - added basic layout boxed layout to app - include bootstrap in index - styled responsive roof

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      "frontend"
+      <div class="container">
+        <div class="row">
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+          <div class="col-6 bbox">
+            One of six columns
+          </div>
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+          <div class="col-6 bbox" id="roof">
+            <div class="roof left"></div>
+            <div class="roof right"></div>
+          </div>
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+          <div class="col-6 bbox">
+            One of six columns
+          </div>
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+          <div class="col-6 bbox">
+            One of six columns
+          </div>
+          <div class="col-3 bbox">
+            One of three columns
+          </div>
+        </div>
+      </div>
     );
   }
 }
