1ef95bca2f6d241fa6b1fa3bc13609a274ac24d1
Horst Jahns <horstjahns@gmail.com>
2018-11-30 02:53:12 +0100

    - flip window to image on hover

--- client/src/components/Window/Window.js
+++ client/src/components/Window/Window.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 import './Window.scss';
 
 class Window extends Component {
+  constructor(){
+    super();
+    this.state = {
+      flipped: false
+    }
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  handleHover(){
+    this.setState({
+      flipped: !this.state.flipped
+    });
+  }
+
   render() {
+    let cardClass = this.state.flipped ? 'card is-flipped' : 'card';
+
     return(
-      <div className="window"></div>
+      <div className="card-wrapper">
+        <div className={cardClass}
+          onMouseEnter={this.handleHover}
+          onMouseLeave={this.handleHover}>
+          <div class="card-face window">
+          </div>
+          <div class="card-face cookie">
+          </div>
+        </div>
+      </div>
     );
   }
 }
